09467d5fd6053eb9774cc025d73ec69f0c8a0db9
rblack42 <rblack@austincc.edu>
2015-12-11 15:49:50 -0600

    travis wants a setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cosc1337_Mandlebrot',
+        verison='0.3',
+        description='C++/GLUT progect for CS1',
+        url='https://github.com/rblack42/cosc1337_Mandlebrot',
+        author='Roie R. Black',
+        author_email='rblack@austincc.edu',
+        license='BSD',
+        packages = ['code'],
+        zip_safe=False)
